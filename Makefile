098955749cbc06565c2b0ad7d09f801f2516fe66
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2025-01-09 16:11:45 +0000

    Add support for OIDC back-channel logout.
    Changes:
      - Add OIDCBackChannelLogoutView view.
      - Add coverage report to CI pipeline.

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
-test:
-	uv run pytest
-
+# Core commands for initialising project and building package
 init:
 	uv sync --no-install-project
 
 build:
 	uv build --no-sources
+
+# Commands for running tests and coverage
+test:
+	uv run pytest
+
+coverage:
+	uv run coverage run --source=govuk_onelogin_django -m pytest
+
+coverage-html:
+	uv run coverage html
--- govuk_onelogin_django/tests/test_views.py
+++ govuk_onelogin_django/tests/test_views.py
@@ -1,11 +1,15 @@
+import logging
 from http import HTTPStatus
 from unittest import mock
 
+import freezegun
 import pytest
 from authlib.jose.errors import InvalidClaimError
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
 from django.core.cache import cache
-from django.test import override_settings
+from django.test import Client, override_settings
 from django.urls import reverse
 
 from govuk_onelogin_django.utils import TOKEN_SESSION_KEY, OneLoginConfig
@@ -13,24 +17,15 @@ from govuk_onelogin_django.views import REDIRECT_SESSION_FIELD_NAME
 
 FAKE_OPENID_CONFIG_URL = "https://oidc.onelogin.gov.uk/.well-known/openid-configuration"
 FAKE_AUTHORIZE_URL = "https://oidc.onelogin.gov.uk/authorize"
+FAKE_JWKS_URI = "https://oidc.onelogin.gov.uk/.well-known/jwks.json"
 
 
 @pytest.fixture(autouse=True, scope="class")
 def correct_settings():
     """Ensure One Login is enabled for all tests"""
-
-    # Clear openid config before and after tests.
-    # Required for the following reasons:
-    #   - The local application has already cached the real values (will break tests)
-    #   - The tests have cached the fake value (will break local development)
-    cache.delete(OneLoginConfig.CACHE_KEY)
-
     with override_settings(
         GOV_UK_ONE_LOGIN_ENABLED=True,
         GOV_UK_ONE_LOGIN_OPENID_CONFIG_URL=FAKE_OPENID_CONFIG_URL,
-        # Required to fix tests (these tests don't really care about SITE_ID)
-        # 2 == The exporter site
-        SITE_ID=2,
     ):
         yield None
 
@@ -46,7 +41,8 @@ def openid_config(requests_mock):
             "token_endpoint": "",
             "userinfo_endpoint": "",
             "end_session_endpoint": "",
-            "issuer": "",
+            "issuer": "https://oidc.integration.account.gov.uk/",
+            "jwks_uri": FAKE_JWKS_URI,
         },
     )
 
@@ -172,3 +168,143 @@ class TestAuthCallbackView:
         response = self.client.get(f"{self.url}?code={auth_code}&state={state}")
         assert response.status_code == HTTPStatus.BAD_REQUEST
         assert caplog.messages[0] == "Unable to validate token"
+
+
+class TestOIDCBackChannelLogoutView:
+    # A test known sub from a now deleted test account
+    TEST_SUB = "urn:fdc:gov.uk:2022:WAWdm8d0W_e8d2Pd9JszlvBDzHEWb1Y4U5pGaWXo6tc"
+    # A test known logout token for a now deleted test account
+    TEST_PAYLOAD = (
+        "eyJraWQiOiI2NDRhZjU5OGI3ODBmNTQxMDZjYTBmM2MwMTczNDFiYzIzMGM0ZjgzNzNmMz"
+        "VmMzJlMThlM2U0MGNjN2FjZmY2IiwidHlwIjoibG9nb3V0K2p3dCIsImFsZyI6IkVTMjU2"
+        "In0.eyJhdWQiOiJ3MFNHMFM4UEotNHZkZVctTmFta3pVcy1EYUEiLCJzdWIiOiJ1cm46Zm"
+        "RjOmdvdi51azoyMDIyOldBV2RtOGQwV19lOGQyUGQ5SnN6bHZCRHpIRVdiMVk0VTVwR2FX"
+        "WG82dGMiLCJpc3MiOiJodHRwczovL29pZGMuaW50ZWdyYXRpb24uYWNjb3VudC5nb3YudW"
+        "svIiwiZXhwIjoxNzM2NzY1NDQzLCJpYXQiOjE3MzY3NjUzMjMsImp0aSI6IjVjYjc1MDZi"
+        "LTgzZWMtNGJkOS1hMjM1LTJkZmRiZmM2ZjA3YyIsImV2ZW50cyI6eyJodHRwOi8vc2NoZW"
+        "1hcy5vcGVuaWQubmV0L2V2ZW50L2JhY2tjaGFubmVsLWxvZ291dCI6e319fQ._JqrsSuX5"
+        "W_jSUAqbIIqE1ziYahlYNLYq_LIbPQbO170OALf6p7m9OQnHbQdlmMod885rqLiDjkbf6B"
+        "qynJSzA"
+    )
+
+    @pytest.fixture(autouse=True)
+    def setup(self, db, requests_mock):
+        # clear cache to prevent JSI error (for different tests using the same payload)
+        cache.clear()
+
+        # CSRF is disabled in one_login:back-channel-logout so check disabling it works.
+        self.client = Client(enforce_csrf_checks=True)
+        self.url = reverse("one_login:back-channel-logout")
+        self.user = User.objects.create_user(
+            self.TEST_SUB, "test_user@example.com", "fake-password"
+        )
+
+        # Fake response from jwks endpoint to known value
+        requests_mock.get(FAKE_JWKS_URI, json=self._jwts_json())
+
+        # Login the created user with a separate client instance to create a session
+        client = Client()
+        assert Session.objects.count() == 0
+        client.force_login(self.user)
+        assert Session.objects.count() == 1
+
+        session = Session.objects.first().get_decoded()
+        assert str(self.user.pk) == session["_auth_user_id"]
+
+    def test_post_only(self):
+        response = self.client.get(self.url)
+
+        assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+
+    @freezegun.freeze_time("10:49:00")
+    def test_oidc_back_channel_logout_successful(self, caplog):
+        # Check the user session has been removed after the back-channel logout request.
+        assert Session.objects.count() == 1
+        response = self.client.post(self.url, data={"logout_token": self.TEST_PAYLOAD})
+        assert Session.objects.count() == 0
+
+        assert response.status_code == HTTPStatus.OK
+
+        # Check duplicate call is caught (invalid claim error).
+        response = self.client.post(self.url, data={"logout_token": self.TEST_PAYLOAD})
+        assert response.status_code == HTTPStatus.OK
+        assert caplog.record_tuples == [
+            (
+                "govuk_onelogin_django.views",
+                logging.ERROR,
+                'OIDCBackChannelLogoutView: Logout Token invalid: invalid_claim: Invalid claim "jti"',
+            )
+        ]
+
+    def test_unknown_error(self, caplog):
+        response = self.client.post(self.url, data={"logout_token": self.TEST_PAYLOAD})
+        assert response.status_code == HTTPStatus.OK
+
+        assert caplog.record_tuples == [
+            (
+                "govuk_onelogin_django.views",
+                logging.ERROR,
+                "OIDCBackChannelLogoutView: Unknown error expired_token: The token is expired",
+            )
+        ]
+
+    def test_decode_error(self, caplog):
+        response = self.client.post(self.url, data={"logout_token": "eyJraWQiOi"})
+        assert response.status_code == HTTPStatus.OK
+
+        assert caplog.record_tuples == [
+            (
+                "govuk_onelogin_django.views",
+                logging.ERROR,
+                "OIDCBackChannelLogoutView: Unable to decode logout token: Invalid input segments length: ",
+            )
+        ]
+
+    @freezegun.freeze_time("10:49:00")
+    def test_oidc_back_channel_logout_user_sub_error(self, caplog):
+        self.user.username = "test_user_name"
+        self.user.save()
+
+        response = self.client.post(self.url, data={"logout_token": self.TEST_PAYLOAD})
+        assert response.status_code == HTTPStatus.OK
+
+        assert caplog.record_tuples == [
+            (
+                "govuk_onelogin_django.views",
+                logging.ERROR,
+                "OIDCBackChannelLogoutView: Unable to log user out with sub: urn:fdc:gov.uk:2022:WAWdm8d0W_e8d2Pd9JszlvBDzHEWb1Y4U5pGaWXo6tc",
+            )
+        ]
+
+    def _jwts_json(self):
+        """This is ok to hardcode as it's a publicly available public key."""
+        return {
+            "keys": [
+                {
+                    "kty": "EC",
+                    "use": "sig",
+                    "crv": "P-256",
+                    "kid": "644af598b780f54106ca0f3c017341bc230c4f8373f35f32e18e3e40cc7acff6",
+                    "x": "5URVCgH4HQgkg37kiipfOGjyVft0R5CdjFJahRoJjEw",
+                    "y": "QzrvsnDy3oY1yuz55voaAq9B1M5tfhgW3FBjh_n_F0U",
+                    "alg": "ES256",
+                },
+                {
+                    "kty": "EC",
+                    "use": "sig",
+                    "crv": "P-256",
+                    "kid": "e1f5699d068448882e7866b49d24431b2f21bf1a8f3c2b2dde8f4066f0506f1b",
+                    "x": "BJnIZvnzJ9D_YRu5YL8a3CXjBaa5AxlX1xSeWDLAn9k",
+                    "y": "x4FU3lRtkeDukSWVJmDuw2nHVFVIZ8_69n4bJ6ik4bQ",
+                    "alg": "ES256",
+                },
+                {
+                    "kty": "RSA",
+                    "e": "AQAB",
+                    "use": "sig",
+                    "kid": "76e79bfc350137593e5bd992b202e248fc97e7a20988a5d4fbe9a0273e54844e",
+                    "alg": "RS256",
+                    "n": "lGac-hw2cW5_amtNiDI-Nq2dEXt1x0nwOEIEFd8NwtYz7ha1GzNwO2LyFEoOvqIAcG0NFCAxgjkKD5QwcsThGijvMOLG3dPRMjhyB2S4bCmlkwLpW8vY4sJjc4bItdfuBtUxDA0SWqepr5h95RAsg9UP1LToJecJJR_duMzN-Nutu9qwbpIJph8tFjOFp_T37bVFk4vYkWfX-d4-TOImOOD75G0kgYoAJLS2SRovQAkbJwC1bdn_N8yw7RL9WIqZCwzqMqANdo3dEgSb04XD_CUzL0Y2zU3onewH9PhaMfb11JhsuijH3zRA0dwignDHp7pBw8uMxYSqhoeVO6V0jz8vYo27LyySR1ZLMg13bPNrtMnEC-LlRtZpxkcDLm7bkO-mPjYLrhGpDy7fSdr-6b2rsHzE_YerkZA_RgX_Qv-dZueX5tq2VRZu66QJAgdprZrUx34QBitSAvHL4zcI_Qn2aNl93DR-bT8lrkwB6UBz7EghmQivrwK84BjPircDWdivT4GcEzRdP0ed6PmpAmerHaalyWpLUNoIgVXLa_Px07SweNzyb13QFbiEaJ8p1UFT05KzIRxO8p18g7gWpH8-6jfkZtTOtJJKseNRSyKHgUK5eO9kgvy9sRXmmflV6pl4AMOEwMf4gZpbKtnLh4NETdGg5oSXEuTiF2MjmXE",
+                },
+            ]
+        }
